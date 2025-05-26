361d574ddbaefc3a33ee321b0f076983f0c530cf
Android Studio <devandroid@ort>
2025-05-26 09:37:16 -0300

    26/5 Parte2

--- source/screens/ScreenHome1.js
+++ source/screens/ScreenHome1.js
@@ -9,7 +9,6 @@ export default function ScreenHome1 () {
       </Image>
       <Text style={styles.text}>Bienvenido a el hogar de los jardineros</Text>
       <Button>Comenzar</Button>
-
     </View>
     </>
   );
